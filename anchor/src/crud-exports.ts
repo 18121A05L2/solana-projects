5fdc659d6cb6d363afcba9d2a54e67340e680bc6
Lakshmi.Sanikommu <59156926+18121A05L2@users.noreply.github.com>
2025-10-26 14:41:53 +0530

    delete unused

--- anchor/src/crud-exports.ts
+++
@@ -1,7 +0,0 @@
-// Here we export some useful types and functions for interacting with the Anchor program.
-import CrudIDL from '../target/idl/crud.json'
-
-// Re-export the generated IDL and type
-export { CrudIDL }
-
-export * from './client/js'
